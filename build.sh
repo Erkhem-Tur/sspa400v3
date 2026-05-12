e37601e282636603996cb1fbfecca5ab1ebc0a37
Erkhem-Tur <2b32oh@gmail.com>
2026-05-12 10:05:54 +0800

    Fix: use STORAGES for Django 6, add collectstatic to build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -e
 pip install -r requirements.txt
 python manage.py migrate
+python manage.py collectstatic --noinput
 python create_departments.py
 python manage.py shell -c "
 from lms.models import Lesson
